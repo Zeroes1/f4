08e67baae08079c80cf4be22ed1f7bc69cced4c8
unxed <ivan.sorokin.tech@gmail.com>
2026-09-24 03:35:25 +0200

    test: isolate rename UI tests from queued tasks

--- internal/app/issue1229_test.go
+++ internal/app/issue1229_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/unxed/f4/internal/panel"
+	"github.com/unxed/f4/internal/paneltest"
 	"github.com/unxed/f4/internal/theme"
 	"github.com/unxed/f4/vfs"
 	"github.com/unxed/vtui"
@@ -57,6 +58,8 @@ func setupRenameConflict(t *testing.T) (*panel.PanelsFrame, *panel.FileSystemPan
 	fsp := pf.Panels[0].(*panel.FileSystemPanel)
 	fsp.Vfs = vfs.NewOSVFS(dir)
 	pf.ActiveIdx = 0
+	paneltest.WaitForLoad(t, fsp)
+	t.Cleanup(func() { drainRenameTasks(t) })
 	return pf, fsp, dir
 }
 
