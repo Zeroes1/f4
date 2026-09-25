60d705ec48f952f5ca5681dc6608e3b805684f74
unxed <ivan.sorokin.tech@gmail.com>
2026-09-25 10:22:09 +0200

    test(app): format terminal condition setup

--- internal/app/command_palette_test.go
+++ internal/app/command_palette_test.go
@@ -67,8 +67,7 @@ func TestCommandPaletteLegacyShortcutSurvivesTheBuiltInBindings(t *testing.T) {
 	vtui.FrameManager.Init(vtui.NewSilentScreenBuf())
 
 	// This test models an idle terminal; an actual running terminal app owns Ctrl+Alt+P.
-	previousNoTerminal, hadNoTerminal := keymap.SetCondition("NoTerminalApp", func() bool { return true })
-	if !hadNoTerminal { t.Cleanup(func() { keymap.SetCondition("NoTerminalApp", previousNoTerminal) }) } else { t.Cleanup(func() { keymap.SetCondition("NoTerminalApp", previousNoTerminal) }) }
+	previousNoTerminal, _ := keymap.SetCondition("NoTerminalApp", func() bool { return true })`n`t.Cleanup(func() { keymap.SetCondition("NoTerminalApp", previousNoTerminal) })) }
 
 	previous := keymap.GlobalHotkeysMgr
 	manager := keymap.NewHotkeyManager(filepath.Join(t.TempDir(), "hotkeys.ini"))
